d6e5a51bf0f6bcadbd445c9fa739e3dc8f76a508
tamybl <tamara.bravo.l@gmail.com>
2018-06-27 16:33:18 -0400

    correccion de errores y mejoras

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Variables Globales
 var number = null;
 var times = 0;
+var numberPlayer, sendTry;
 
 // Generar un numero es 1 a 100, guardarlo en una variable
 
@@ -16,51 +17,63 @@ let random = () => {
 
 function gamePlay () {
     random();
-    var sendTry = document.getElementById('send-try');
+    sendTry = document.getElementById('send-try');
     sendTry.addEventListener('click', function () {
-        var numberPlayer = document.getElementById('number-player').value;
-        document.getElementById('number-player').value = '';
+        numberPlayer = document.getElementById('number-player');
+        numberPlayer.focus();
+
         if (times < 9) {
-            if(numberPlayer > number) {
+            if(numberPlayer.value > number) {
                 times++;
                 message.innerHTML = `<p class="red">¡Equivocado!</p> <p class="warning-txt">Muy Alto! :(</p>`;
-                showtry.innerHTML += `<span>${numberPlayer}</span>`;
+                showtry.innerHTML += `<span>${numberPlayer.value}</span>`;
               
             }
-            if (numberPlayer < number) {
+            if (numberPlayer.value < number) {
+                if(numberPlayer.value === '') {
+                    numberPlayer.value = 0;
+                }
                 times++;
                 message.innerHTML = `<p class="red">¡Equivocado!</p> <p class="warning-txt">Muy Bajo! :(</p>`;
-                showtry.innerHTML += `<span>${numberPlayer}</span>`;
+                showtry.innerHTML += `<span>${numberPlayer.value}</span>`;
             } 
-            if ((/^[0-9]*$/gm).test(numberPlayer) === false || numberPlayer === '') {
+            if ((/^[0-9]*$/gm).test(numberPlayer.value) === false) {
                 times++;
                 message.innerHTML = `<p class="red">¡Equivocado!</p> <p class="warning-txt">El texto que ingresaste no es válido</p>`;
                 showtry.innerHTML += `<span>NaN</span>`;
 
             }
-            if (numberPlayer == number){
+            if (numberPlayer.value == number){
                 message.innerHTML = `<p class="green">Acertaste!</p>`;
+                numberPlayer.disabled = true;
+                sendTry.disabled = true;
                 message.appendChild(btnReset);
                 reset.addEventListener('click', () => {
-                    times = 0;
-                    message.innerHTML = '';
-                    showtry.innerHTML = '';
-                    random();
+                    resetOptions ();
                 });
             }
         }
         else {
-            message.innerHTML = `Perdiste!`;
+            message.innerHTML = `<p style="color: rgb(68, 68, 68);">Perdiste!<p>`;
+            showtry.innerHTML += `<span>${numberPlayer.value}</span>`;
             message.appendChild(btnReset);
+            numberPlayer.disabled = true;
+            sendTry.disabled = true;
             reset.addEventListener('click', () => {
-                times = 0;
-                message.innerHTML = '';
-                showtry.innerHTML = '';
-                random();
+                resetOptions ();
             })
         }
-
+        numberPlayer.value = '';
     })
+}
+
+function resetOptions () {
+    times = 0;
+    message.innerHTML = '';
+    showtry.innerHTML = '';
+    random();
+    numberPlayer.disabled = false;
+    sendTry.disabled = false;
 }
 
 gamePlay();
